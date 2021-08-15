7efbfa17bff33e99bdc9c9dd09384d3b49c7301c
Pieter Koornhof <pieterkoornhof@Pieters-Mac-mini.local>
2021-08-15 22:45:52 +0400

    API upsert

--- src/y/api.clj
+++ src/y/api.clj
@@ -17,20 +17,20 @@
 
 (defn- wrap-json [request]
   (let [has-body?     (contains? request :body)
-        body-request? (contains? #{:post :put} (:method request))]
+        body-request? (contains? #{:post :put :patch} (:method request))]
     (cond-> request
       true                          (assoc-in [:headers "Content-Type"] "application/json; charset=utf-8")
       (and body-request? has-body?) (update :body json/generate-string))))
 
 
 (defn- parse-response [response]
-  (if (= 200 (:status response))
+  (if (#{200 209} (:status response))
     (cond-> response
       (string? (:body response))
       (update :body json/parse-string keyword)
       true
       (get-in [:body :data]))
-    (throw (ex-info "Request Error" response))))
+    (throw (ex-info "Request Error" (dissoc response :opts)))))
 
 
 (defn- client
@@ -58,6 +58,17 @@
 (defn get-transactions
   "since-date '2021-07-25' "
   [account-id since-date]
+  (->>
+    (client
+      {:method :get
+       :url    (str "/budgets/" (config/budget-id) "/accounts/" account-id "/transactions?since_date=" since-date)})
+    :transactions
+    (map (fn [t] (select-keys t [:id :account_id :date :amount :memo])))))
+
+
+(defn save-transactions
+  [transactions]
   (client
-    {:method :get
-     :url    (str "/budgets/" (config/budget-id) "/accounts/" account-id "/transactions?since_date=" since-date)}))
+    {:method :patch
+     :url    (str "/budgets/" (config/budget-id) "/transactions")
+     :body {:transactions transactions}}))
